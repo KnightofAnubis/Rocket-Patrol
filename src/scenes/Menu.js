076e79593f1af25835996ad4076540e16aeda921
Kelsey Melott <knightsofanubis352@gmail.com>
2023-04-17 20:12:44 -0700

    trying this...

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -5,12 +5,12 @@ class Menu extends Phaser.Scene {
     
     preload() {
         //loading audio
-        this.load.audio('sfx_select', '/assets/blip_select12.wav');
-        this.load.audio('sfx_explosion', '/assets/explosion38.wav');
-        this.load.audio('sfx_rocket', '/assets/rocket_shot.wav');
+        this.load.audio('sfx_select', 'assets/blip_select12.wav');
+        this.load.audio('sfx_explosion', 'assets/explosion38.wav');
+        this.load.audio('sfx_rocket', 'assets/rocket_shot.wav');
     }
 
-    
+
     create() {
         //real menu
         let menuConfig = {
